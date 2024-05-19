591ef36c9c9becb9f8f6427f52636e29cd204ea9
aryarna <aryanrajpandey780@gmail.com>
2024-05-19 17:35:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+var express=require("express")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/config')
+var db=mongoose.connection
+db.on('error',()=> console.log("Error in Connecting to Database"))
+db.once('open',()=> console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res) => {
+    var name= req.body.name
+    
+    var password=req.body.password
+
+    var data={
+        "name":name,
+        
+        "password":password
+    }
+    db.collection('mst').insertOne(data,(err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Succesfully")
+    })
+  //  return res.redirect('signup_successful.html')
+})
+
+app.get("/",(req,res) => {
+    res.set({
+        "Allow-acces-Allow-Origin":'*'
+    })
+    return res.redirect('index.html')
+}).listen(3000);
+
+console.log("Listening on port 3000")
